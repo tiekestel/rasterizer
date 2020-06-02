c99ef2d175b146754b59c4120eee8b4d02791ee7
Tieke <tieke.stel@gmail.com>
2020-06-02 17:30:54 +0200

    Basic project done

--- template_P2_2020/SceneGraph.cs
+++ template_P2_2020/SceneGraph.cs
@@ -1,25 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenTK;
 
 namespace Template
 {
 	class SceneGraph
 	{
-		ParentMesh camera;
+		ParentMesh world;
+		ParentMesh teapot;
+
 		class ParentMesh
 		{
 			List<ParentMesh> child_meshes = new List<ParentMesh>();
 			Mesh mesh;
 			Texture texture;
+			Matrix4 localTransform;
 
-			public ParentMesh(Mesh _mesh, Texture _texture)
+			public ParentMesh(Mesh _mesh, Texture _texture, Matrix4 _localTransform)
 			{
 				mesh = _mesh;
 				texture = _texture;
+				localTransform = _localTransform;
 			}
 
 			//Add child meshes to parent mesh
@@ -28,22 +32,29 @@ namespace Template
 				child_meshes.Add(child_mesh);
 			}
 
-			public void Render(Matrix parentMatrix, Shader shader)
+			public void Render(Matrix4 parentMatrix, Shader shader)
 			{
+				//Combine matrices
+				Matrix4 finalTransform = localTransform * parentMatrix;
+				mesh.Render(shader, finalTransform, texture);
 				//Render child meshes
 				foreach(ParentMesh p in child_meshes)
 				{
-					p.Render(null, shader);
+					p.Render(finalTransform, shader);
 				}
 			}
 		}
 
-		public SceneGraph(Matrix camera)
+		public SceneGraph()
 		{
-			ParentMesh world = new ParentMesh(new Mesh("../../assets/floor.obj"), new Texture("../../assets/wood.jpg"));
-			ParentMesh teapot = new ParentMesh(new Mesh("../../assets/teapot.obj"), new Texture("../../assets/wood.jpg"));
-			world.Add(teapot);
+			world = new ParentMesh(new Mesh("../../assets/floor.obj"), new Texture("../../assets/tiles.jpg"), Matrix4.CreateScale(4.0f) * Matrix4.CreateFromAxisAngle(new Vector3(0, 1, 0), 0));
+			teapot = new ParentMesh(new Mesh("../../assets/teapot.obj"), new Texture("../../assets/wood.jpg"), Matrix4.CreateScale(0.5f) * Matrix4.CreateFromAxisAngle(new Vector3(0, 1, 0), 0));
+		}
+
+		public void Render(Matrix4 camera, Shader shader)
+		{
 			world.Render(camera, shader);
+			teapot.Render(camera, shader);
 		}
 	}
 }
--- template_P2_2020/shaders/fs.glsl
+++ template_P2_2020/shaders/fs.glsl
@@ -3,6 +3,7 @@
 // shader input
 in vec2 uv;						// interpolated texture coordinates
 in vec4 normal;					// interpolated normal
+in vec3 position;
 uniform sampler2D pixels;		// texture sampler
 
 // shader output
@@ -11,5 +12,22 @@ out vec4 outputColor;
 // fragment shader
 void main()
 {
-    outputColor = texture( pixels, uv ) + 0.5f * vec4( normal.xyz, 1 );
+    //Ambient light
+    outputColor = texture( pixels, uv );
+
+    //Diffuse light
+    vec3 directionalLight = normalize(vec3(1, -1, 0));
+    float angle = 0.1;
+    //float angle = max(dot(directionalLight, normal.xyz), 0);
+
+    //Specular light
+    float specular = 0.0;
+    if(angle > 0) {
+        vec3 reflection = reflect(directionalLight, normal.xyz);
+        vec3 view = normalize(-position);
+        float specularAngle = max(dot(reflection, view), 0);
+        specular = pow(specularAngle, 40);
+    }
+
+    outputColor += vec4(angle, angle, angle, 0) + vec4(specular, specular, specular, 0);
 }
